705273124f0c6fbaaaa002cd73e31148a0fa95ff
Donny <orionsring@tamu.edu>
2019-05-05 15:11:54 -0700

    Add files via upload

--- blog.py
+++ blog.py
@@ -6,7 +6,7 @@ def update():
     record = db.blog(request.args(0)) or redirect(URL('post'))
     form = SQLFORM(db.blog, record)
     if form.process().accepted:
-        response.flash = T('Record Updated')
+        response.flash = T('record Updated')
     else:
         response.flash = T('Please complete the form.')
     return locals()
@@ -18,14 +18,24 @@ def display_form():
         redirect(URL('thanks'))
     elif form.errors:
         response.flash = 'form has errors'
-    else:
-        response.flash = 'please fill out the form'
     return locals()
 
+def thanks():
+    msg = "Thank you for submitting your post"
+    return locals()
+
+@auth.requires_membership('blog_poster')
 def post():
     form = SQLFORM(db.blog).process()
     return locals()
 
+@auth.requires_membership('blog_poster')
+def post():
+    form = SQLFORM(db.blog).process()
+    return locals()
+
+@auth.requires_login()
 def view():
     rows = db(db.blog).select(orderby=~db.blog.id)
     return locals()
+
