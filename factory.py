c4c783b58f1ba255c21069928deb13d3fb5c0c47
Donny <orionsring@tamu.edu>
2019-05-25 21:15:16 -0600

    Add files via upload

--- factory.py
+++ factory.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 # try something like 
 
+def isPrime(num):
+    counter = 2
+    while counter < num - 1:
+        if num%counter==0:
+            return False
+        counter = counter + 1
+    return True
+
+def primeform():
+    answer = False
+    form = SQLFORM.factory(
+        Field('enter_number', requires=IS_INT_RANGE(10, 10000))
+        )
+    if form.process().accepted:
+        response.flash = 'form accepted'
+        session.enter_number = form.vars.enter_number
+        answer = isPrime(int(session.enter_number))
+    elif form.errors:
+        response.flash = 'form has errors'
+    return locals()
+	   
 def form():
     form = SQLFORM.factory(
         Field('your_name', requires=IS_NOT_EMPTY()),
